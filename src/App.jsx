967bd5e61d378d008851a9055759597a94a7e557
dbfu <fudebao@byteplan.com>
2024-01-30 21:16:16 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('')
 
   let SHAKETHRESHOLD = 4000;
   let lastUpdate = 0;
@@ -26,16 +26,20 @@ function App() {
   }, [])
 
   function deviceMotionHandler(eventData) {
-    let acceleration = eventData.accelerationIncludingGravity;
+    let acceleration = eventData.acceleration;
     let curTime = new Date().getTime();
+
+    setCount(`${acceleration.x}-${acceleration.y}-${acceleration.z}`);
+
     if (curTime - lastUpdate > 50) {
       let diffTime = curTime - lastUpdate;
       lastUpdate = curTime;
       x = acceleration.x;
       y = acceleration.y;
       z = acceleration.z;
+
+     
       let speed = (Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime) * 10000;
-      setCount(x);
       if (speed > SHAKETHRESHOLD) {
         setTimeout(() => {
           alert("恭喜你，摇到漂亮妹纸一枚!"); // Do something
